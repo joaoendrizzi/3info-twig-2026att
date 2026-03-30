e3f503c99a6bfdfed0bc7aebfd76f93f72d44340
João Rafael Endrizzi <joaoendrizzi@gmail.com>
2026-03-30 17:00:17 -0300

    excluir e editar

--- carregar_pdo.php
+++ carregar_pdo.php
@@ -1,3 +1,3 @@
 <?php
 // carregar_pdo.php
-$pdo = new PDO('mysql:host=localhost;dbname=3info', 'root');
+$pdo = new PDO('mysql:host=localhost;dbname=endrizzi', 'root');
---
+++ jogos_excluir.php
@@ -0,0 +1,27 @@
+<?php
+//jogos_excluir.php
+require('carregar_pdo.php'); 
+if ($_SERVER['REQUEST_METHOD'] =='POST') {
+    $id = (int) $_POST["id"] ?? false;
+    if ($id) {
+        $excluir = $pdo->prepare('DELETE FROM jogos WHERE id = :id');
+        $excluir->bindParam(':id', $id);
+        $excluir->execute();
+    }
+    header("location:jogos.php");
+    die;
+}
+
+require "carregar_twig.php";
+
+$dados = $pdo->prepare('SELECT * FROM jogos WHERE id = :id');
+$dados->execute([':id'=> $id]);
+
+if ($dados->rowCount() != 1) {
+    header("Location: jogos.php");
+    die;
+}
+$jogo = $dados->fetch(PDO::FETCH_ASSOC);
+echo $twig->render("jogos_excluir.html", [
+    "jogo" => $jogo
+]);
